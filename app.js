3a690edad6553fc2fa4eb2c299a995769d65e80d
Jackslaps <jackslaps@pop-os.localdomain>
2019-04-13 14:32:54 -0400

    Added heroku stuff

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config;
 
 const express = require('express');
 const hbs     = require('hbs');
@@ -16,31 +17,4 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res, next) => {
-  punkAPI.getBeers()
-  //beerData is just a placeholder in can be any word
-    .then(beerData => {
-      //console.log(beerData)
-  //beerData is what i will use to call {{#each beerData}} {{/each}}    
-      res.render('beers', { beerData });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
-
-app.get('/randombeer', (req, res, next) => {
-
-  punkAPI.getRandom()
-  .then(beerData => {
-    //console.log(beerData[0])
-    res.render('randombeer', { randomBeer:beerData[0] });
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  
-});
-
-
-app.listen(3000, () => console.log("Listening on port 3000:)"));
+app.listen(process.env.PORT, () => console.log("Listening on port 3000 :)"));
